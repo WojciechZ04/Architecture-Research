5aa210de8710ec6e7a444b1824619ba504933e96
Wojciech Zieliński <wojtekz.6776@gmail.com>
2024-06-11 07:48:07 +0200

    basic project boilerplate: created routing, pages, header component

--- component-based/src/App.js
+++ component-based/src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Tasks from "./pages/Tasks";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/projects" element={<Projects />}></Route>
+          <Route path="/tasks" element={<Tasks />}></Route>
+          <Route path="/" element={<Home />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ component-based/src/pages/Tasks.jsx
@@ -0,0 +1,8 @@
+export default function Tasks() {
+	  return (
+	<div>
+	  <h2>Tasks</h2>
+	  <p>Tasks will go here</p>
+	</div>
+  );
+}
