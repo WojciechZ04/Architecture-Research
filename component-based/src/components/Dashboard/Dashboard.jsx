60c52d42a5985a194de52a89f601a766a03a0761
Wojciech Zieliński <wojtekz.6776@gmail.com>
2024-06-13 20:25:26 +0200

    add progress bar

--- component-based/src/components/Dashboard/Dashboard.jsx
+++ component-based/src/components/Dashboard/Dashboard.jsx
@@ -1,5 +1,19 @@
 import "./Dashboard.css";
 import Task from "../Task/Task";
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+import { styled } from '@mui/material/styles';
+
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+  height: 10,
+  borderRadius: 5,
+  [`&.${linearProgressClasses.colorPrimary}`]: {
+    backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+  },
+  [`& .${linearProgressClasses.bar}`]: {
+    borderRadius: 5,
+    backgroundColor: theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8',
+  },
+}));
 
 export default function Dashboard() {
   return (
@@ -8,7 +22,7 @@ export default function Dashboard() {
         <button>Project 1</button>
         <button>Project 2</button>
       </div>
-      <p>There will be progress bar</p>
+      <BorderLinearProgress variant="determinate" value={40} />
       <div>
         <table>
           <thead>
