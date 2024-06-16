faa4992236160e5d0116d545fed06e0e287db500
Wojciech Zieliński <wojtekz.6776@gmail.com>
2024-06-16 10:03:43 +0200

    remake navbar: make it sidebar, animated

--- component-based/src/App.js
+++ component-based/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
@@ -8,16 +8,21 @@ import Navbar from "./components/Navbar";
 import NotFound from "./pages/NotFound";
 
 function App() {
+  const [mainMargin, setMainMargin] = useState('85px');
+  
+  
   return (
     <div className="App">
       <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/projects" element={<Projects />}></Route>
-          <Route path="/tasks" element={<Tasks />}></Route>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="*" element={<NotFound />}></Route>
-        </Routes>
+        <Navbar setMainMargin={setMainMargin}/>
+        <div id="main" style={{ marginLeft: mainMargin }}>
+          <Routes>
+            <Route path="/projects" element={<Projects />}></Route>
+            <Route path="/tasks" element={<Tasks />}></Route>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Routes>
+        </div>
       </Router>
     </div>
   );
