9e7a5f6a23a6bf55e335c3a1e7281c85aa502a10
Wojciech Zieliński <wojtekz.6776@gmail.com>
2024-06-20 09:47:32 +0200

    create assets directory with project data.json; create api to get that data

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const app = express();
 
 app.use(cors());
 
-app.get('/api', (req, res) => {
-  res.json({ "users": ['user1', 'user2', 'user3'] });
+app.get('/api/data', (req, res) => {
+  const filePath = path.join(__dirname, '..', 'assets', 'data.json');
+  res.sendFile(filePath);
 });
 
 app.listen(5000, () => { console.log('Server is running on port 5000') });
