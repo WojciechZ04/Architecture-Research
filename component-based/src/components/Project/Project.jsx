7aa0c2f736ae89d13e7f2679cda85e8fcfbde798
Wojciech Zieliński <wojtekz.6776@gmail.com>
2024-06-30 22:17:58 +0200

    update projects display, add indicator

--- component-based/src/components/Project/Project.jsx
+++ component-based/src/components/Project/Project.jsx
@@ -3,10 +3,13 @@ import "./Project.css";
 export default function Project({ project }) {
   return (
     <div className="project">
-      <h2 className="project__title">{project.name}</h2>
-      <button>Details</button>
+      <div className="indicator"></div>
+      <p className="project__title">{project.name}</p>
+      <p>15 May 2024</p>
+      <p>Milestone 2</p>
+      {/* <button>Details</button>
       <button>Tasks</button>
-      <button>Teams</button>
+      <button>Teams</button> */}
     </div>
   );
 }
