4ad7ac2a21fd0d36a16282f4d015888e85329e4e
WojciechZ04 <wojciechz.6776@gmail.com>
2024-08-06 13:19:20 +0200

    home page: fix calendar positioning and styling; quick logout from dashboard; on click display calendar on smaller screens

--- component-based/src/components/BorderLinearProgress.jsx
+++ component-based/src/components/BorderLinearProgress.jsx
@@ -3,12 +3,10 @@ import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgr
 
 const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
   height: 10,
-  borderRadius: 5,
   [`&.${linearProgressClasses.colorPrimary}`]: {
     backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 300 : 800],
   },
   [`& .${linearProgressClasses.bar}`]: {
-    borderRadius: 5,
     backgroundColor: theme.palette.mode === 'light' ? '#34cf5d' : '#308fe8',
   },
 }));
--- component-based/src/pages/Home/Home.jsx
+++ component-based/src/pages/Home/Home.jsx
@@ -4,12 +4,27 @@ import Project from "./components/Project";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import { useNavigate } from "react-router-dom";
+import useSignOut from "react-auth-kit/hooks/useSignOut"; 
 import "./Home.css";
 
 export default function Home() {
   const [tasks, setTasks] = useState([]);
   const [projects, setProjects] = useState([]);
   const [user, setUser] = useState({});
+  const [isCalendarVisible, setIsCalendarVisible] = useState(false);
+
+  const signOut = useSignOut();
+
+  const navigate = useNavigate();
+  const handleSignOut = () => {
+    signOut();
+    navigate("/login");
+  };
+
+  const toggleCalendarVisibility = () => {
+    setIsCalendarVisible(!isCalendarVisible);
+  };
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -78,11 +93,16 @@ export default function Home() {
         <div className="home-sidebar">
           <div className="home-sidebar-header">
             <i className="material-icons">notifications</i>
-            <i className="material-icons">logout</i>
+            <i className="material-icons" onClick={toggleCalendarVisibility}>calendar_month</i>
+            <i className="material-icons" onClick={() => handleSignOut()}>logout</i>
           </div>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DateCalendar />
-          </LocalizationProvider>
+          <div className={`calendar-container ${isCalendarVisible ? 'visible' : ''}`}>
+            <div className="calenar-wrapper">
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DateCalendar className="calendar" />
+              </LocalizationProvider>
+            </div>
+          </div>
         </div>
       </div>
     </div>
